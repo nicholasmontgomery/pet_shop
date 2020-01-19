c5de2bb7475b2ac865c63a8739f92ca898e20fb0
nicholasmontgomery <na_montgomery@yahoo.co.uk>
2020-01-19 17:29:18 +0000

    Third upload

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -42,6 +42,18 @@ def find_pet_by_name (pet_shop, pet_name)
   return nil
 end
 
-def customer_cash(array)
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet_name === pet[:name]
+    pet_shop[:pets].delete(pet)
+    end
+  end
+end
 
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+def customer_cash(array)
+  return array[:cash]
 end
